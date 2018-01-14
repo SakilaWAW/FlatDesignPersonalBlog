47c1a18513ea48f54a85da0559c0eff548314786
SakilaWAW <pccp0909@163.com>
2018-01-14 20:30:40 +0800

    使用google字体服务

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,17 @@
 </template>
 
 <script>
-import 'normalize.css';
+  import 'reset-css';
 
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
-
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+  html {
+    font-family: "Open Sans",arial,x-locale-body,sans-serif;
+    font-size: 100px;
+  }
 </style>
