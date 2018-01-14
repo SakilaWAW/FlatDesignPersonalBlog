a95ecf322c8c772172c93ee54d40acc253c5442e
SakilaWAW <pccp0909@163.com>
2018-01-14 20:31:07 +0800

    初步完成header

--- src/components/HeaderArea.vue
+++ src/components/HeaderArea.vue
@@ -1,26 +1,104 @@
 <template>
   <div class="head-bar">
     <nav id="nav-bar">
-      <div class="logo">Logo</div>
+      <div class="logo"><a href="#">T<>T</a></div>
       <ul class="title-href">
-        <li>标签1</li>
-        <li>标签2</li>
-        <li>标签3</li>
-        <li>标签4</li>
+        <li><a href="#">朱雀三生</a></li>
+        <li><a href="#">玄阳</a></li>
+        <li><a href="#">近水楼台先得月</a></li>
+        <li><a href="#">逻辑</a></li>
       </ul>
     </nav>
+    <div class="banner" :style="bannerBackground">
+      <div class="welcome-block">
+        <span class="title-txt">Talk is Cheap. Show me the code.</span>
+        <span class="sub-title-txt">Never Give up.</span>
+        <button>Now about me</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  const bannerImg = require('../assets/banner_rain.jpg');
+
   export default {
     name: 'header-area',
+    data() {
+      return {
+        bannerBackground: {
+          backgroundImage: `url( ${bannerImg} )`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        },
+      };
+    },
   };
 </script>
 
 <style lang="scss" scoped>
-#nav-bar {
-  display: flex;
-  flex-direction: row;
-}
+  #nav-bar {
+    position: relative;
+    font-size: 0;
+    display: block;
+    background: gray;
+    height: 0.4rem;
+    .logo {
+      display: inline-block;
+      font-size: 0.3rem;
+      margin-left: 0.1rem;
+      height: 0.4rem;
+      line-height: 0.4rem;
+    }
+    .title-href {
+      position: absolute;
+      right: 0.1rem;
+      top: 0;
+      display: flex;
+      align-items: center;
+      font-size: 0.2rem;
+      li {
+        margin: 0 0.1rem;
+        display: inline-block;
+      }
+    }
+    a {
+      text-decoration: none;
+      color: white;
+      display: inline-block;
+      line-height: 0.4rem;
+      height: 0.4rem;
+    }
+  }
+  .banner {
+    height: 10rem;
+    width: 100%;
+    .welcome-block {
+      margin: 0 auto;
+      font-size: 0.2rem;
+      width: 4rem;
+      height: 4rem;
+      position: relative;
+      top: 5rem;
+      transform: translateY(-50%);
+      text-align: center;
+      .title-txt {
+        line-height: 0.4rem;
+        display: block;
+      }
+      .sub-title-txt {
+        line-height: 0.35rem;
+        margin-bottom: 0.2rem;
+        display: block;
+      }
+      button {
+        border: none;
+        background: #999;
+        line-height: 0.4rem;
+        height: 0.4rem;
+        width: 1.2rem;
+        font-weight: bold;
+      }
+    }
+  }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,8 +11,6 @@
   import ContentArea from './ContentArea';
   import FooterArea from './FooterArea';
 
-  require('normalize.css');
-
   export default {
     name: 'Home',
     components: {
@@ -23,20 +21,6 @@
   };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+
 </style>
